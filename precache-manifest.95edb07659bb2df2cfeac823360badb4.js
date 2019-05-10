33cf24dd14b9a3a8a8580fea43ec03be5f90dc53
Amit Sengar <amitsengar@yahoo.com>
2019-05-10 14:05:11 +0530

    Updates

---
+++ precache-manifest.95edb07659bb2df2cfeac823360badb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb930f8558eebf736a1969959aa698e6",
+    "url": "/chai/index.html"
+  },
+  {
+    "revision": "c3ac03581ef909954cc1",
+    "url": "/chai/static/css/main.1a491f25.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/chai/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c3ac03581ef909954cc1",
+    "url": "/chai/static/js/main.4c8d2b4e.chunk.js"
+  },
+  {
+    "revision": "14627f3bc525b61b5c39",
+    "url": "/chai/static/js/runtime~main.a2eb498d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chai/static/media/logo.5d5d9eef.svg"
+  }
+]);
